107b56f0acaa259a8a686b13138429144a40b05e
김승우 <95275793+KimSeungWoo18@users.noreply.github.com>
2023-04-29 13:25:27 +0900

    Initial Commit

---
+++ MVVMinPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMinPractice
+//
+//  Created by seungwooKim on 2023/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
