e29b3d1135e6db0a88a9b0a6205b390d58f98d83
김승우 <95275793+KimSeungWoo18@users.noreply.github.com>
2023-05-26 14:45:40 +0900

    ViewController에 모든 todo 로직 넣기(MVC)

--- MVVMinPractice/ViewController.swift
+++ MVVMinPractice/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    var todos = ["집안 일", "공부하기", "TIL 쓰기"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
+        
     }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        todos.append("할일")
+        tableview.performBatchUpdates {
+            tableview.insertRows(at: [IndexPath(row: todos.count-1, section: 0)], with: .automatic)
+        }
+    }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = todos[indexPath.row]
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+//        cell?.accessoryType = cell?.accessoryType == UITableViewCell.AccessoryType.none ? .checkmark : .none
+        cell?.imageView?.image = cell?.imageView?.image == nil ? UIImage(systemName: "checkmark") : nil
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+}
 
+extension String {
+    func strikeThrough() -> NSAttributedString {
+            let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+            return attributeString
+        }
 }
 
